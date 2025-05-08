f3d7a8bc009483d9063e8f8468fe18dc702c404b
Enzo Rojas <enzo.e.rojas@gmail.com>
2025-05-08 20:11:08 -0300

    delete, put and patch routes for supplier model added

--- backend/routes/supplier_routes.py
+++ backend/routes/supplier_routes.py
@@ -42,3 +42,80 @@ def add_supplier():
         db.session.rollback()
         print(f'Unexpected error: {error}')
         return jsonify({'error': 'Error adding supplier'}), 500
+    
+@supplier.route('/api/delete_supplier', methods=['DELETE'])
+def del_supplier():
+    supplier = Supplier.query.get(id)
+
+    if not supplier:
+        return jsonify({'message': 'Supplier not found'}), 404
+    
+    try:
+        db.session.delete(supplier)
+        db.session.commit()
+        return jsonify({'message': 'Supplier deleted successfully'}), 200
+    
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({'error': 'Error deleting supplier'}), 500
+    
+@supplier.route('/api/update_supplier', methods=['PUT'])
+def update_supplier():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({'error': 'No data received'}, 400)
+    
+    supplier = Supplier.query.get(id)
+
+    if not supplier:
+        return jsonify({'error': 'Supplier not found'}), 404
+    
+    try:
+        supplier.rut = data.get('rut', supplier.rut)
+        supplier.name = data.get('name', supplier.name)
+        supplier.address = data.get('address', supplier.address)
+        supplier.phone = data.get('phone', supplier.phone)
+        supplier.website = data.get('website', supplier.website)
+
+        db.session.commit()
+
+        return jsonify({'message': 'Supplier updated successfully',
+                        'supplier': supplier.serialize()}), 200
+    
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({'error': 'Error updating supplier'}), 500
+    
+@supplier.route('/api/patch_supplier', methods=['PATCH'])
+def patch_supplier():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({'error': 'No data received'}), 400
+    
+    supplier = Supplier.query.get(id)
+
+    if not supplier:
+        return jsonify({'error': 'Supplier not found'}), 404
+    
+    try:
+        if 'rut' in data:
+            supplier.rut = data['rut']
+        if 'name' in data:
+            supplier.name = data['name']
+        if 'address' in data:
+            supplier.address = data['address']
+        if 'phone' in data:
+            supplier.phone = data['phone']
+        if 'website' in data:
+            supplier.website = data['website']
+
+        db.session.commit()
+
+        return jsonify({'message': 'Supplier patched successfully',
+                        'supplier': supplier.serialize()}), 200
+    
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({'error': 'Error patching supplier'}), 500
