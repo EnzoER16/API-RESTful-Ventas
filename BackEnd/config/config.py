537fe139f1179c48449740a250f9ef2570a48067
Maty <agueromatiase@gmail.com>
2025-05-09 18:44:33 -0300

    update

--- BackEnd/config/config.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-# Cargar variables del archivo .env
-load_dotenv()
-
-# Leer la URI desde el entorno
-DATABASE_CONNECTION_URI = os.getenv('DATABASE_URI')
